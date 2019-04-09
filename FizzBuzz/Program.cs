3a3f4302dd08a10aeacb902c3ad3d55afe3c16a1
chase <chase.weber47@gmail.com>
2019-04-08 21:37:18 -0400

    first commit

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                var printme = "";
+                if (i % 3 == 0)
+                {
+                    printme += "Fizz";
+                }
+                if (i % 5 == 0)
+                {
+                    printme += "Buzz";
+                }
+                if (printme == "")
+                {
+                    printme += i;
+                }
+                Console.WriteLine(printme);
+            }
+        }
+    }
+}
